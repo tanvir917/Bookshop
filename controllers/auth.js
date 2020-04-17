c050ee66b6b4db2cd7188be48f91cb8c49d17c88
tanvir <mdtanvirislams@gmail.com>
2020-04-17 20:29:43 +0600

    signup page validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const {validationResult} = require('express-validator/check')
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -78,7 +79,15 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        });
+    }
     User.findOne({email: email})
     .then(userDoc => {
         if(userDoc){
