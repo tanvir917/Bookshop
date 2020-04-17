044cefa218fe6c660c28c1c333177fcc0b53533d
tanvir <mdtanvirislams@gmail.com>
2020-04-18 01:28:32 +0600

    validation to login and sanitizing data

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,12 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+          email: '',
+          password: ''
+        },
+        validationError: []
     });
 };
 
@@ -44,18 +49,42 @@ exports.getSignup = (req, res, next) => {
             password: '',
             confirmPassword: ''
         },
-        validationError: []
+        validationError: [] 
     });
 };
 
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        console.log(errors.array());
+        return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationError: errors.array()
+        });
+    }
+
     User.findOne({email: email})
         .then(user => {
             if(!user) {//didnt find user
-                req.flash('error', 'Invalid email or password.');
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                      email: email,
+                      password: password
+                    },
+                    validationError: []
+                });
             }
             //if user find
             bcrypt.compare(password, user.password)
@@ -70,8 +99,16 @@ exports.postLogin = (req, res, next) => {
                         res.redirect('/');
                     });
                 }
-                req.flash('error', 'Invalid email or password.');
-                res.redirect('/login');//invalid password
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                      email: email,
+                      password: password
+                    },
+                    validationError: []
+                });
             })
             .catch(err => {
                 console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,20 @@ router.get('/login',authController.getLogin);
 
 router.get('/signup',authController.getSignup);
 
-router.post('/login',authController.postLogin);
+router.post(
+    '/login',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .normalizeEmail(),
+        body('password', 'Password has to be valid.')
+            .isLength({min: 5})
+            .isAlphanumeric()
+            .trim(),
+    ],
+    authController.postLogin
+);
 
 router.post(
     '/signup', 
@@ -31,13 +44,17 @@ router.post(
                     return Promise.reject('E-mail exists already, please pick a different one.');
                 }
             })
-        }), 
+        })
+        .normalizeEmail(), 
         body('password', 
         'Please enter a password with only numbers and text and at least 5 characters.'
         )
         .isLength({min: 5})
-        .isAlphanumeric(),
-        body('confirmPassword').custom((value, { req }) => {
+        .isAlphanumeric()
+        .trim(),
+        body('confirmPassword')
+        .trim()
+        .custom((value, { req }) => {
             if(value !== req.body.password) {
                 throw new Error('Passwords have to match!');
             }
