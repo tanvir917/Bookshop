19b48c344955510e9faca594752f5ceb150d236e
tanvir <mdtanvirislams@gmail.com>
2020-04-18 21:32:04 +0600

    downloading and setting file type headers

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,8 @@
 const Product = require('../models/product');// import class
 //const Cart = require('../models/cart');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
 
 //from shop.js
 exports.getProducts = (req, res, next)=>{
@@ -198,9 +200,24 @@ exports.getOrders = (req, res, next) => {
         });
 };
 
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+        if(err) {
+            return next(err);
+        }
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+        res.send(data);
+    })
+};
+
 /*exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout'
     });
 };*/
+
--- routes/shop.js
+++ routes/shop.js
@@ -24,4 +24,6 @@ router.post('/create-order', isAuth, shopController.postOrder);
 router.get('/orders', isAuth, shopController.getOrders);
 //router.get('/checkout', shopController.getCheckout);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice)
+
 module.exports = router; 
