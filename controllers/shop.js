8f87898cd3f6d24c2d461effb6725582d91ca1f4
tanvir <mdtanvirislams@gmail.com>
2020-04-18 22:37:20 +0600

    preloading data

--- controllers/shop.js
+++ controllers/shop.js
@@ -202,16 +202,33 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
     const orderId = req.params.orderId;
-    const invoiceName = 'invoice-' + orderId + '.pdf';
-    const invoicePath = path.join('data', 'invoices', invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-        if(err) {
-            return next(err);
+    Order.findById(orderId).then(order => {
+        if(!order){
+            return next(new Error('No order found.'));
         }
+        //logged in user
+        if(order.user.userId.toString() !== req.user._id.toString()){
+            return next(new Error('Unauthorised user'));
+        }
+        const invoiceName = 'invoice-' + orderId + '.pdf';
+        const invoicePath = path.join('data', 'invoices', invoiceName);
+        /*fs.readFile(invoicePath, (err, data) => {
+            if(err) {
+                return next(err);
+            }
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+            res.send(data);
+        })*/
+        const file = fs.createReadStream(invoicePath);
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-        res.send(data);
+        file.pipe(res);
+
+    }).catch(err => {
+        console.log(err);
     })
+   
 };
 
 /*exports.getCheckout = (req, res, next) => {
