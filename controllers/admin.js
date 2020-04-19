2a1b1e878f76a85c50e97a0d220f25db72fb53e3
tanvir <mdtanvirislams@gmail.com>
2020-04-20 01:35:03 +0600

    sending data to backend with async req(delete a product on client side)

--- controllers/admin.js
+++ controllers/admin.js
@@ -203,8 +203,8 @@ exports.getProducts = (req, res, next) => {
 };
 
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId).then(product => {
         if(!product){
             return next(new Error('Product not found.'));
@@ -213,11 +213,10 @@ exports.postDeleteProduct = (req, res, next) => {
         return Product.deleteOne({ _id: prodId, userId: req.user._id });
     }).then(() => {
         console.log('Destroyed Product');
-        res.redirect('/admin/products');
+        //res.redirect('/admin/products');
+        res.status(200).json({message: 'Success!'})
     })
     .catch(err => {
-        const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
+        res.status(500).json({message: 'Deleting Product failed.'});
     });    
 }
--- routes/admin.js
+++ routes/admin.js
@@ -52,7 +52,7 @@ router.post('/edit-product',
  isAuth, 
  adminController.postEditProduct);
 
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 //exports.routes = router;
 //exports.products = products; 
